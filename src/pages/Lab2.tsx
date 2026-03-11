05c48aee4467310fd245a2ce3f7d0b86eb815252
Tran Tuan Hung <hungtran092006@gmail.com>
2026-03-11 15:07:44 +0700

    Refactor data structure in StudentTable for improved readability

--- src/pages/Lab2.tsx
+++ src/pages/Lab2.tsx
@@ -20,44 +20,12 @@ const columns = [
 ];
 
 const data = [
-  {
-    key: 1,
-    id: 1,
-    name: "Nam",
-    age: 20,
-    major: "IT",
-  },
-  {
-    key: 2,
-    id: 2,
-    name: "Linh",
-    age: 21,
-    major: "Business",
-  },
-  {
-    key: 3,
-    id: 3,
-    name: "Hà",
-    age: 19,
-    major: "Design",
-  },
-  {
-    key: 4,
-    id: 4,
-    name: "Hung",
-    age: 19,
-    major: "Design",
-  },
-  {
-    key: 5,
-    id: 5,
-    name: "Hungg",
-    age: 19,
-    major: "Design",
-  },
-
+  {key: 1,id: 1,name: "Nam", age: 20,major: "IT",},
+  {key: 2,id: 2,name: "Linh",age: 21,major: "Business",},
+  {key: 3, id: 3, name: "Hà", age: 19,major: "Design",},
+  {key: 4,id: 4,name: "Hung",age: 19,major: "Design",},
+  {key: 5,id: 5,name: "Hungg",age: 19,major: "Design",},
 ];
-
 export default function StudentTable() {
   return (
     <div style={{ width: "500px", margin: "20px auto" }}>
